5967325e1449aa086fcf3bf14b49512012995dcb
Sharon Mwenda <smkathambi@gmail.com>
2022-06-05 12:17:04 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts){
+    for(let i= 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const party = 'surprise';
+
+function writeCards(names, party) {
+    let newArray = [];
+    for (let i =0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${party} gift!`);
+    }
+    return newArray;
+}
+console.log(writeCards(names, party));
+
+
+function countDown(positiveNumber){
+    while(positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+}
+console.log(countDown(10));
